294696d63dd6bc6db0a69d0f2439590df557d609
VinothBen <bvinothkumark@gmail.com>
2018-05-06 11:50:56 +0530

    Changed to ionic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,30 @@
-import { NgModule } from '@angular/core';
-import { IonicApp, IonicModule } from 'ionic-angular';
-import { AppComponent } from './app.component';
-import { HomePage } from './pages/home.page';
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
 
 @NgModule({
-  imports: [
-    IonicModule.forRoot(AppComponent)
-  ],
   declarations: [
-    AppComponent,
+    MyApp,
     HomePage
   ],
-  entryComponents: [
-    AppComponent,
-    HomePage    
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp)
   ],
-  bootstrap: [IonicApp]
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
 })
-export class AppModule { }
+export class AppModule {}
